b5f5a5e59de2ad1a5d2d16d31b0fe20df039575a
Alexander <formysite.ru@yandex.ru>
2023-04-12 15:41:28 +0300

    start

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Урок 8.4. Работа с файлами с помощью PHP</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>
+            Урок 8.4. Работа с файлами с помощью PHP
+        </h1>
+    </header>
+    <main>
+        <section class="">
+            <div class="code_php">
+                <?php
+                $nickname = ' Александр!';
+                $hello = 'Привет ';
+                $offer = 'На данном уроке мы научимся работать с файлами с помощью языка программирования PHP. <br> Разберем примеры, как создавать файл, записывать в файл и читать файл';
+
+                echo '<h2 class="title_php">' . $hello . $nickname . '</h2>';
+
+                echo '<h4 class="title_php1">' . $offer . '</h4>';
+                ?>
+            </div>
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+        </section>
+        <section class=""></section>
+        <section class=""></section>
+        <section class=""></section>
+        <section class=""></section>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
